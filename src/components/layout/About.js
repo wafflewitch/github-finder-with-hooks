184e694a7dbf8d723ad232b406305b0d808db3e9
wafflewitch <jessicapruitt717@gmail.com>
2020-05-10 10:21:17 -0500

    Create new repo of github-finder project to prepare for refactoring project with hooks.

---
+++ src/components/layout/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const About = () => (
+  <Fragment>
+    <h1>About this app</h1>
+    <p>An app to find Github users.</p>
+  </Fragment>
+);
+
+export default About;
