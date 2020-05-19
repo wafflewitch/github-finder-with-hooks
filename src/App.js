6a41ead279a43e2a9ce789c99000e82f894e0d53
wafflewitch <jessicapruitt717@gmail.com>
2020-05-19 18:17:15 -0500

    Create pages directory. Move About component to pages directory. Create Home component in pages directory. Import About and Users components into Home component. Refactor App.js to route to Home component instead of rendering props.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
-import About from './components/layout/About';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -21,16 +20,7 @@ class App extends Component {
               <div className='container'>
                 <Alert />
                 <Switch>
-                  <Route
-                    exact
-                    path='/'
-                    render={(props) => (
-                      <Fragment>
-                        <Search />
-                        <Users />
-                      </Fragment>
-                    )}
-                  />
+                  <Route exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/user/:login' component={User} />
                 </Switch>
